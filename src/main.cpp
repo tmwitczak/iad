e05d2b75f0cb1adaedb1cd5c6069db999c0cc481
Tomasz Witczak <TomaszMWitczak@gmail.com>
2019-05-17 21:03:03 +0200

    Remove Python embedding support

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,115 @@
+///////////////////////////////////////////////////////////////////// | Includes
+#include "multi-layer-perceptron.hpp"
 #include <iostream>
 #include <ctime>
-#include "perceptron-layer.hpp"
-#include "multi-layer-perceptron.hpp"
-#include "sigmoid.hpp"
-#include "parametric-rectified-linear-unit.hpp"
 
 using namespace std;
+using namespace NeuralNetworks;
 using Vector = Eigen::VectorXd;
 
+////////////////////////////////////////////////////////////// | Project: iad-2a
 int main()
 {
-    using namespace NeuralNetworks;
+    constexpr int
+            n = 4,
+            h = 2,
+            m = 4;
+
+    MultiLayerPerceptron::initialiseRandomNumberGenerator(static_cast<int>
+                                                          (time(nullptr)));
+    MultiLayerPerceptron mlp {{ n, h, m },
+                              std::vector<bool>(2, true) };
+
+    std::vector<TrainingExample> trainingData
+            {
+                    {
+                            (Vector { n } << 1.0, 0.0, 0.0, 0.0).finished(),
+                            (Vector { m } << 1.0, 0.0, 0.0, 0.0).finished()
+                    },
+                    {
+                            (Vector { n } << 0.0, 1.0, 0.0, 0.0).finished(),
+                            (Vector { m } << 0.0, 1.0, 0.0, 0.0).finished()
+                    },
+                    {
+                            (Vector { n } << 0.0, 0.0, 1.0, 0.0).finished(),
+                            (Vector { m } << 0.0, 0.0, 1.0, 0.0).finished()
+                    },
+                    {
+                            (Vector { n } << 0.0, 0.0, 0.0, 1.0).finished(),
+                            (Vector { m } << 0.0, 0.0, 0.0, 1.0).finished()
+                    }
+            };
+
+    int const numberOfEpochs = 10000;
+    double const costGoal = 0.00001;
+    double const learningCoefficient = 0.2;
+    double const learningCoefficientChange = -0.1;
+    double const momentumCoefficient = 0.8;
+    bool const shuffleTrainingData = true;
+    int const epochInterval = 500;
+
+    MultiLayerPerceptron::TrainingResults trainingResults
+            = mlp.train(trainingData,
+                        numberOfEpochs, costGoal,
+                        learningCoefficient, learningCoefficientChange,
+                        momentumCoefficient,
+                        shuffleTrainingData,
+                        epochInterval);
+
+    std::cout << "training results" << std::endl;
+    std::cout << "epoch interval: " << trainingResults.epochInterval
+              << std::endl;
+    for (auto const &i : trainingResults.costPerEpochInterval)
+        std::cout << i << std::endl;
+
+    MultiLayerPerceptron::TestingResults testingResults
+            = mlp.test(trainingData);
+
+    std::cout << "testing results" << std::endl;
+    std::cout << "global cost: " << testingResults.globalCost << std::endl;
+    for (auto const &i : testingResults.testingResultsPerExample)
+    {
+        std::cout << "neurons\n";
+        for (auto const &j : i.neurons)
+            std::cout << ">" << std::endl
+                      << j << std::endl;
+        std::cout << "targets\n";
+        std::cout << i.targets << std::endl;
+        std::cout << "errors\n";
+        for (auto const &j : i.errors)
+            std::cout << ">" << std::endl
+                      << j << std::endl;
+        std::cout << "cost\n";
+        std::cout << i.cost << std::endl;
+    }
+
+    mlp.saveToFile("multi-layer-perceptron.mlp");
+    MultiLayerPerceptron loadedFromFile { "multi-layer-perceptron.mlp" };
+
+    using std::cout;
+    using std::endl;
+    cout << endl;
+    cout << mlp((Vector { n } << 1.0, 0.0, 0.0, 0.0).finished()) << "\n\n";
+    cout << loadedFromFile((Vector { n } << 1.0, 0.0, 0.0, 0.0).finished()) <<
+         "\n\n";
+//    cout << mlp((Vector { n } << 0.0, 1.0, 0.0, 0.0).finished()) << "\n\n";
+//    cout << mlp((Vector { n } << 0.0, 0.0, 1.0, 0.0).finished()) << "\n\n";
+//    cout << mlp((Vector { n } << 0.0, 0.0, 0.0, 1.0).finished()) << "\n\n";
+
+
+//    using namespace NeuralNetworks;
+//    Sigmoid sigmoid;
+//    std::unique_ptr<Cloneable<ActivationFunction>> cloneable
+//            = std::make_unique<Sigmoid>();
+//    Sigmoid sigmoidCopyAssignment = sigmoid;
+//    PerceptronLayer layer1 { 3, 4, Sigmoid {}};
+//    PerceptronLayer layer2 { 3, 4, RectifiedLinearUnit {}};
+//    PerceptronLayer layer3 { 3, 4 };
+
+
+    return 0;
+}
+
 
 //    PerceptronLayer::initialiseRandomSeed(time(nullptr));
 //    std::vector<PerceptronLayer> layers;
@@ -58,75 +157,3 @@ int main()
 //    cout << "errors:\n\n" << errorsPerLayer.at(1) << "\n\n";
 //    cout << "targets:\n\n" << example2.outputs << "\n\n";
 //-------------------------------------------------------------
-
-    //-------------------------------------------------------------
-
-
-    constexpr int
-            n = 4,
-            h = 2,
-            m = 4;
-
-    MultiLayerPerceptron::initialiseRandomNumberGenerator(static_cast<int>
-                                                          (time(nullptr)));
-    MultiLayerPerceptron mlp { std::vector<int> { n, h, m }};
-
-    std::vector<TrainingExample> trainingData
-            {
-                    {
-                            (Vector { n } << 1.0, 0.0, 0.0, 0.0).finished(),
-                            (Vector { m } << 1.0, 0.0, 0.0, 0.0).finished()
-                    },
-                    {
-                            (Vector { n } << 0.0, 1.0, 0.0, 0.0).finished(),
-                            (Vector { m } << 0.0, 1.0, 0.0, 0.0).finished()
-                    },
-                    {
-                            (Vector { n } << 0.0, 0.0, 1.0, 0.0).finished(),
-                            (Vector { m } << 0.0, 0.0, 1.0, 0.0).finished()
-                    },
-                    {
-                            (Vector { n } << 0.0, 0.0, 0.0, 1.0).finished(),
-                            (Vector { m } << 0.0, 0.0, 0.0, 1.0).finished()
-                    }
-            };
-
-    int const numberOfEpochs = 10000;
-    double const costGoal = 0.00001;
-    double const learningCoefficient = 0.2;
-    double const learningCoefficientChange = -0.1;
-    double const momentumCoefficient = 0.8;
-    bool const shuffleTrainingData = true;
-
-    mlp.train(trainingData,
-              numberOfEpochs, costGoal,
-              learningCoefficient, learningCoefficientChange,
-              momentumCoefficient,
-              shuffleTrainingData);
-
-    mlp.saveToFile("multi-layer-perceptron.mlp");
-    MultiLayerPerceptron loadedFromFile { "multi-layer-perceptron.mlp" };
-
-    using std::cout;
-    using std::endl;
-    cout << endl;
-    cout << mlp((Vector { n } << 1.0, 0.0, 0.0, 0.0).finished()) << "\n\n";
-    cout << loadedFromFile((Vector { n } << 1.0, 0.0, 0.0, 0.0).finished()) <<
-         "\n\n";
-//    cout << mlp((Vector { n } << 0.0, 1.0, 0.0, 0.0).finished()) << "\n\n";
-//    cout << mlp((Vector { n } << 0.0, 0.0, 1.0, 0.0).finished()) << "\n\n";
-//    cout << mlp((Vector { n } << 0.0, 0.0, 0.0, 1.0).finished()) << "\n\n";
-
-
-//    using namespace NeuralNetworks;
-//    Sigmoid sigmoid;
-//    std::unique_ptr<Cloneable<ActivationFunction>> cloneable
-//            = std::make_unique<Sigmoid>();
-//    Sigmoid sigmoidCopyAssignment = sigmoid;
-//    PerceptronLayer layer1 { 3, 4, Sigmoid {}};
-//    PerceptronLayer layer2 { 3, 4, RectifiedLinearUnit {}};
-//    PerceptronLayer layer3 { 3, 4 };
-
-
-    return 0;
-}
