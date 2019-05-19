00122d3dbcae8c43514452c58f651db0a581c168
Tomasz Witczak <TomaszMWitczak@gmail.com>
2019-05-19 14:22:26 +0200

    Add script for overall data preparation

---
+++ src/prepare-data-sets.py
@@ -0,0 +1,48 @@
+import os
+import shutil
+import subprocess
+
+print('Preparing data sets:')
+
+print('> Unzipping \'data.zip\'')
+subprocess.call('unzip-data.py',
+                shell=True)
+
+print('> Converting:')
+print('  - \'iris.csv\'')
+subprocess.call('convert-classification-data.py'
+                + ' -i ./original-data/iris.csv'
+                + ' -c 4'
+                + ' -o ./prepared-data/iris.csv'
+                + ' -n ./prepared-data/iris-normalised.csv'
+                + ' -s ./prepared-data/iris-standardised.csv',
+                shell=True)
+
+print('  - \'seeds.csv\'')
+subprocess.call('convert-classification-data.py'
+                + ' -i ./original-data/seeds.csv'
+                + ' -c 7'
+                + ' -o ./prepared-data/seeds.csv'
+                + ' -n ./prepared-data/seeds-normalised.csv'
+                + ' -s ./prepared-data/seeds-standardised.csv',
+                shell=True)
+
+print('  - \'digits-train.csv\'')
+subprocess.call('convert-classification-data.py'
+                + ' -i ./original-data/digits-train.csv'
+                + ' -c 0'
+                + ' -o ./prepared-data/digits-train.csv',
+                shell=True)
+
+print('  - \'digits-test.csv\'')
+subprocess.call('convert-classification-data.py'
+                + ' -i ./original-data/digits-test.csv'
+                + ' -c 0'
+                + ' -o ./prepared-data/digits-test.csv',
+                shell=True)
+
+print('> Deleting \'original-data\'')
+shutil.rmtree('./original-data')
+
+print('> Renaming \'prepared-data\' to \'data\'')
+os.rename('./prepared-data', './data')
